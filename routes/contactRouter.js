0ccab675de0b559ae69b4f84a055db520b008dd6
Kateryna <k.borysenko.kyiv@gmail.com>
2024-02-18 01:31:46 +0200

    Implement retrieval of data from MongoDB using Mongoose methods

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -2,24 +2,29 @@ import express from 'express';
 import {
   getAllContacts,
   getOneContact,
-  deleteContact,
   createContact,
   updateContact,
+  updateFavorite,
+  deleteContact,
 } from '../controllers/contactControllers.js';
 import validateRequestBody from '../middleware/validateRequestBody.js';
+import checkObjectId from '../middleware/checkObjectId.js';
 import controllerWrapper from '../helpers/controllerWrapper.js';
 import { createContactSchema, updateContactSchema } from '../schemas/contactSchemas.js';
+import { updateFavoriteSchema } from '../schemas/updateFavoriteSchema.js';
 
 const contactRouter = express.Router();
 
 contactRouter.get('/', controllerWrapper(getAllContacts));
 
-contactRouter.get('/:id', controllerWrapper(getOneContact));
-
-contactRouter.delete('/:id', controllerWrapper(deleteContact));
+contactRouter.get('/:id', checkObjectId, controllerWrapper(getOneContact));
 
 contactRouter.post('/', validateRequestBody(createContactSchema), controllerWrapper(createContact));
 
-contactRouter.put('/:id', validateRequestBody(updateContactSchema), controllerWrapper(updateContact));
+contactRouter.put('/:id', checkObjectId, validateRequestBody(updateContactSchema), controllerWrapper(updateContact));
+
+contactRouter.patch("/:id/favorite", checkObjectId, validateRequestBody(updateFavoriteSchema), controllerWrapper(updateFavorite));
+
+contactRouter.delete('/:id', checkObjectId, controllerWrapper(deleteContact));
 
 export default contactRouter;
