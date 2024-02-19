24c127c4333e189f0caef344da147b6b60eb0e5e
Kateryna <k.borysenko.kyiv@gmail.com>
2024-02-20 00:45:41 +0200

    Update handleMongooseError

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,4 +1,6 @@
-export const handleMongooseError = (err, data, next) => {
-  err.status = 400;
-  next();
-}
+export const handleMongooseError = (error, data, next) => {
+  const { name, code } = error;
+  const status = (name === "MongoServerError" && code === 11000) ? 409 : 400;
+  error.status = status;
+  next()
+};
