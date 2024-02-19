ee8dd6ebe27838cbb6b5a404bc38587d1c343da7
Kateryna <k.borysenko.kyiv@gmail.com>
2024-02-19 23:51:44 +0200

    Add userModel

---
+++ models/userModel.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { handleMongooseError } from '../helpers/handleMongooseError.js';
+
+const userSchema = mongoose.Schema({
+  password: {
+    type: String,
+    required: [true, 'Set password for user'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  token: String
+}, { versionKey: false, timestamps: true });
+
+contactSchema.post('save', handleMongooseError);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
