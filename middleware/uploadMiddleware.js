ac52d357ad17c92912d1361215f678f8f39004ff
Kateryna <k.borysenko.kyiv@gmail.com>
2024-02-23 15:11:51 +0200

    Add a check to only upload images

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -13,8 +13,23 @@ const multerConfig = multer.diskStorage({
   }
 });
 
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = mimetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new Error('Images only!'), false);
+  }
+}
+
 const upload = multer({
-  storage: multerConfig
+  storage: multerConfig,
+  fileFilter
 });
 
 export default upload;
