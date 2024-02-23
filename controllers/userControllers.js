6c3597ba40841157dcf3e48e892eba3478c8ab0b
Kateryna <k.borysenko.kyiv@gmail.com>
2024-02-23 14:55:39 +0200

    Add Upload User Avatar

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,9 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
+import gravatar from 'gravatar';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import User from '../models/userModel.js';
 import HttpError from '../helpers/HttpError.js';
 import generateToken from '../helpers/generateToken.js';
@@ -14,8 +18,9 @@ export const registerUser = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email, { s: '250' });
 
-  await User.create({ ...req.body, password: hashPassword });
+  await User.create({ ...req.body, password: hashPassword, avatarURL });
 
   res.status(201).json({
     email,
@@ -78,3 +83,44 @@ export const updateSubscription = async (req, res) => {
 
   res.status(200).json({ message: `You have updated your subscription to ${updatedUser.subscription}` });
 };
+
+export const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+
+  //временный путь к файлу и его имя
+  const __filename = fileURLToPath(import.meta.url);
+  //Users/katerynaborysenko/Desktop/phonebook-backend/controllers/userControllers.js
+  const __dirname = path.dirname(__filename);
+  //Users/katerynaborysenko/Desktop/phonebook-backend/controllers
+
+  //куда переместить файл
+  const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+  //Users/katerynaborysenko/Desktop/phonebook-backend/public/avatars
+  const { path: tmpUpload, originalname } = req.file;
+  //  req.file: {
+  //   fieldname: 'avatar',
+  //     originalname: 'avatar.png',
+  //       encoding: '7bit',
+  //         mimetype: 'image/png',
+  //           destination: '/Users/katerynaborysenko/Desktop/phonebook-backend/tmp',
+  //             filename: 'avatar.png',
+  //               path: '/Users/katerynaborysenko/Desktop/phonebook-backend/tmp/avatar.png',
+  //                 size: 54707
+  // }
+
+  //присваиваем новое имя файлу
+  const filename = `${_id}_${originalname}`;
+  //путь куда переместить файл
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tmpUpload, resultUpload);
+
+  //запись в db
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.status(200).json({
+    message: 'Avatar updated successfully',
+    avatarURL
+  });
+  //результат обновления аватара и сохранения в db, загруженный файл хранится в папке public/avatars
+}
