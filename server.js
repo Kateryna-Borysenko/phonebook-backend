dcbe661045d33bc20e7d8c2d44c17a765c03e801
Kateryna <k.borysenko.kyiv@gmail.com>
2024-03-18 10:11:03 +0200

    Cors & Max-Age of cookie

--- server.js
+++ server.js
@@ -15,11 +15,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan('tiny'));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static("public"))
+app.use(express.static('public'));
 
 app.use('/api/contacts', contactRouter);
 app.use('/api/users', userRouter);
@@ -34,9 +39,12 @@ app.use((err, req, res, next) => {
     .json({ message: err.message || 'Internal Server Error' });
 });
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(PORT);
-}).catch((err) => {
-  console.error(err.message);
-  process.exit(1);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.error(err.message);
+    process.exit(1);
+  });
